5fb8d1402b2a99875e504cf247256cfd6abd4394
MarieGomezCode <espitiaangela740@gmail.com>
2021-05-26 09:17:15 -0500

    Se creo el proyecto 3

---
+++ PROYECTO_FINAL_ANGELA_ESPITIAA/js/dinamico.js
@@ -0,0 +1,30 @@
+/*function determinarSigno(){
+	let dia=parseInt(document.getElementById("dia").value)
+	dia=parseInt(dia)
+
+	let mes=parseInt(document.getElementById("mes").value)
+	
+	if (((dia.value>=21 && dia.value<=31) && mes.value==1)||((dia.value>=1 && dia.value<=18) && dia.value==2)) {
+	//Eres acuario
+}if((dia.value>=19 && dia.value<=28 && mes.value==2)||(dia.value>=1 && dia.value<=20 && dia.value==3))  {
+    //eres de Piscisi
+ 
+}if (dia.value==29 && mes.value==2){
+    	alert("sos del año bisiesto y sos Piscis");
+}if((dia.value>=21 && dia.value<=31 && mes.value==3)||(dia.value>=1 && dia.value<=20 && dia.value==4))  {
+    //eres de acuario
+}if((dia.value>=21 && dia.value<=18 && mes.value==7)||(dia.value>=21 && dia.value<=18 && dia.value==8))  {
+    //eres de acuario
+}if ((dia.value>=21 && dia.value<=18 && mes.value==9)||(dia.value>=21 && dia.value<=18 && dia.value==10))  {
+    //eres de acuario
+}if((dia.value>=22 && dia.value<=18 && mes.value==11)||(dia.value>=21 && dia.value<=18 && dia.value==1))  {
+    //eres de acuario
+}
+
+}*/
+
+
+
+
+
+
